357998a361004d7b3611fd9c7385051e4ae3086c
UjwlJain <ujjawalpugalia09@gmail.com>
2024-05-03 10:27:39 +0530

    new blog app
    FIRST MERN STACK PROJECT

---
+++ config/db.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("Connected to Mongodb Database")
+} catch (error) {
+        console.log(`MONGO Connection Error= ${error}`)
+}
+};
+module.exports = connectDB;
